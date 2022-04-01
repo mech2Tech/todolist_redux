046db0a3183d759c0b6c2cc1e4b29c2c28c47f8c
Mech2Tech <none>
2022-03-31 23:29:52 -0400

    Add feature

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,23 +1,49 @@
 import React from 'react';
 import { connect } from 'react-redux';
+
+
 class TodoList extends React.Component {
+  
+  handleChange = (e) => this.props.dispatch({
+    type: 'HANDLE_CHANGE',
+    title: e.target.value
+  })
+  addItem = (e) => this.props.dispatch({
+    type: 'ADD_ITEM'
+
+  })
   render(){
-    console.log(this.props.items)
+    console.log(this.props)
     return(
       <>
         <div className = 'mainPage'>
           <h1>todos</h1>
-          <ul className = "todoListItem">
-            {this.props.items.map((item, index) => (
-              <li key = {index}>
-                <span> {item.title} </span>
+          <input 
+            type = 'text'
+            placeholder = 'what needs to be done?'
+            value={this.props.title}
+            onBlur = {this.handleChange}
+          />
+          <button onClick = {this.addItem}>Enter Item</button>
+          <ul>
+            {this.props.items.map((item, id) => 
+              <li key = {id}>
+                {item.title}
               </li>
-            ))}
+            )}
           </ul>
         </div>
       </>
     );
   }
 }
-const mapStoreToProps = (store) => ({ items: store.items, title: store.newItem });
+const mapStoreToProps = (store) => ({ items: store });
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     addItem: function() {
+//       dispatch ({type: 'ADD_ITEM'})
+//     }
+//   }
+// }
+
 export default connect(mapStoreToProps)(TodoList);
